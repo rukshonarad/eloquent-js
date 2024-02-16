6cfc6160bcb02ab7e11724c153d59a994ca1c338
Rukshona Rad <rukshona.rad@gmail.com>
2024-02-16 09:03:51 -0500

    Chapter one binary search string comperison

---
+++ Chapter-1/index.js
@@ -0,0 +1,30 @@
+// Chapter-1 Values, Types, and Operators
+
+console.log(8 * null);
+// → 0
+console.log("5" - 1);
+// → 4
+console.log("5" + 1);
+// → 51
+console.log("five" * 2);
+// → NaN
+console.log(false == 0);
+// → true
+console.log(null == undefined);
+// → true
+console.log(null == 0);
+// → false
+console.log(NaN == NaN);
+// → false
+
+console.log(null || "user");
+// → user
+console.log("Agnes" || "user");
+// → Agnes
+
+// binary operators for arithmetic (+, -, *, /, and %)
+// string concatenation (+)
+// comparison (==, !=, ===, !==, <, >, <=, >=)
+// logic (&&, ||)
+// unary operators (- to negate a number, ! to negate logically, and typeof to find a value’s type)
+// ternary operator (?:) to pick one of two values based on a third value.
