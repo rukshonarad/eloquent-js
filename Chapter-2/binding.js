93ca6bd4fcf8242cb5ee0be3a8ba550506caabb2
Rukshona Rad <rukshona.rad@gmail.com>
2024-02-17 08:56:12 -0500

    Solve binding

---
+++ Chapter-2/binding.js
@@ -0,0 +1,26 @@
+let caught = 5 * 5;
+// -> 25
+console.log(caught);
+let ten = 10;
+console.log(ten * ten);
+
+let mood = "light";
+console.log(mood);
+// -> light
+mood = "dark";
+console.log(mood);
+// -> dark
+
+let luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+// ->105
+
+let one = 1,
+    two = 2;
+console.log(one + two);
+// -> 3
+var name = "Ayda";
+const greeting = "Hello ";
+console.log(greeting + name);
+// -> Hello Ayda
