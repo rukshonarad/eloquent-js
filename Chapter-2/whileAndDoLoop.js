f8e41e6a584f7adce97957333f4814314f250dd9
Rukshona Rad <rukshona.rad@gmail.com>
2024-02-26 08:50:48 -0500

    Solve while and do loop

---
+++ Chapter-2/whileAndDoLoop.js
@@ -0,0 +1,28 @@
+console.log(0);
+console.log(2);
+console.log(4);
+console.log(6);
+console.log(8);
+console.log(10);
+console.log(12);
+console.log("----------------");
+let number = 0;
+while (number <= 12) {
+    console.log(number);
+    number = number + 2;
+}
+console.log("----------------");
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+    result = result * 2;
+    counter = counter + 1;
+}
+console.log(result);
+console.log("----------------");
+
+// let yourName;
+// do {
+//     yourName = prompt("Who are you?");
+// } while (!yourName);
+// // console.log(yourName);
