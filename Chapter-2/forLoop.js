151e719e4a14b1b1e0be41943c9db4f0f46d65ac
Rukshona Rad <rukshona.rad@gmail.com>
2024-02-26 08:53:57 -0500

    Solve for loop

---
+++ Chapter-2/forLoop.js
@@ -0,0 +1,9 @@
+for (let number = 0; number <= 12; number = number + 2) {
+    console.log(number);
+}
+console.log("--------------");
+let result = 1;
+for (let counter = 0; counter < 10; counter = counter + 1) {
+    result = result * 2;
+}
+console.log(result);
---
+++ Chapter-2/intending.js
@@ -0,0 +1,6 @@
+if (false != true) {
+    console.log("That makes sense.");
+    if (1 < 2) {
+        console.log("No surprise there.");
+    }
+}
