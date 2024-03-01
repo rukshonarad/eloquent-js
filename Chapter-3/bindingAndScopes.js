66efb779b37670a59b2ea1907a13b80beaeb7585
Rukshona Rad <rukshona.rad@gmail.com>
2024-03-01 00:15:30 -0500

    Solve bindingAndScope

--- Chapter-3/bindingAndScopes.js
+++ Chapter-3/bindingAndScopes.js
@@ -21,3 +21,22 @@ const power = function (base, exponent) {
 
 console.log(power(2, 10));
 // → 1024
+let x = 10;
+if (true) {
+    let y = 20;
+    var z = 30;
+    console.log(x + y + z);
+    // → 60
+}
+// y is not visible here
+console.log(x + z);
+// → 40
+const halve = function (n) {
+    return n / 2;
+};
+
+let n = 10;
+console.log(halve(100));
+// → 50
+console.log(n);
+// → 10
