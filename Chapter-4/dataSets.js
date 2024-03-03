f06c74b63b2242e86b5be85b5325e981a7b5f77a
Rukshona Rad <rukshona.rad@gmail.com>
2024-03-03 00:34:26 -0500

    Solve objects

---
+++ Chapter-4/dataSets.js
@@ -0,0 +1,58 @@
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[2]);
+// → 5
+console.log(listOfNumbers[0]);
+// → 2
+console.log(listOfNumbers[2 - 1]);
+// → 3
+
+// Methods
+let doh = "Doh";
+console.log(typeof doh.toUpperCase);
+// → function
+console.log(doh.toUpperCase());
+// → DOH
+
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+// → [1, 2, 3, 4, 5]
+console.log(sequence.pop());
+// → 5
+console.log(sequence);
+// → [1, 2, 3, 4]
+
+// OBJECTS//
+let day1 = {
+    squirrel: false,
+    events: ["work", "touched tree", "pizza", "running"]
+};
+console.log(day1.squirrel);
+// → false
+console.log(day1.wolf);
+// → undefined
+day1.wolf = false;
+console.log(day1.wolf);
+// → false
+let descriptions = {
+    work: "Went to work",
+    "touched tree": "Touched a tree"
+};
+
+let anObject = { left: 1, right: 2 };
+console.log(anObject.left);
+// → 1
+delete anObject.left;
+console.log(anObject.left);
+// → undefined
+console.log("left" in anObject);
+// → false
+console.log("right" in anObject);
+// → true
+console.log(Object.keys({ x: 0, y: 0, z: 2 }));
+// → ["x", "y", "z"]
+let objectA = { a: 1, b: 2 };
+Object.assign(objectA, { b: 3, c: 4 });
+console.log(objectA);
+// → {a: 1, b: 3, c: 4}
